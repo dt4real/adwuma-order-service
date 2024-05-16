1e2efcc9849da901a837747fcfc2469998cb513b
dt4real <dotchere95@gmail.com>
2024-05-16 15:48:13 +0200

    Implement order routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,13 @@
+import { verifyGatewayRequest } from '@dt4real/adwuma-common';
 import { Application } from 'express';
+import { healthRoutes } from '@order/routes/health';
+import { orderRoutes } from '@order/routes/order';
+
+const BASE_PATH = '/api/v1/order';
 
 const appRoutes = (app: Application): void => {
-  console.log(app);
+  app.use('', healthRoutes());
+  app.use(BASE_PATH, verifyGatewayRequest, orderRoutes());
 };
 
 export { appRoutes };
