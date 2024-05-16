cc11a93afce4375a3ad1224e8e731ac211aa0650
dt4real <dotchere95@gmail.com>
2024-05-16 15:45:36 +0200

    Implement order controllers

---
+++ src/controllers/notification/get.ts
@@ -0,0 +1,11 @@
+import { getNotificationsById } from '@order/services/notification.service';
+import { IOrderNotifcation } from '@dt4real/adwuma-common';
+import { Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+
+const notifications = async (req: Request, res: Response): Promise<void> => {
+  const notifications: IOrderNotifcation[] = await getNotificationsById(req.params.userTo);
+  res.status(StatusCodes.OK).json({ message: 'Notifications', notifications });
+};
+
+export { notifications };
